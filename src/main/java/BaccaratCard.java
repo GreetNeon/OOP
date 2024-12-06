e126b981774381cb2195330ef62b8c95fd40a99e
GreetNeon <97176150+GreetNeon@users.noreply.github.com>
2024-12-06 14:05:01 +0000

    Delete src directory

--- src/main/java/BaccaratCard.java
+++
@@ -1,8 +0,0 @@
-// TODO: Implement the BaccaratCard class in this file
-public class BaccaratCard extends Card {
-
-    public BaccaratCard(Card.Rank r, Card.Suit s){
-        super(r, s);
-    }
-
-}
